998cc6ac92dd82bc88d0a5137c63460be2f2a6eb
Kaandieepan Sathiyaseelan <kaandi2002@gmail.com>
2024-08-13 21:51:24 +0530

    Updated the Tabs and JSON for DFS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,16 +26,20 @@ export class AppComponent {
     vsid:'',
     dsid:'',
     directory:'',
+    assetId:''
   }
   show_json = false;
 
   CurrDate = new Date();
   month = this.CurrDate.getMonth()+1;
+  date2 = this.CurrDate.getDate()+2;
   date = "0" + this.CurrDate.getDate();
+  date3 = "0" + this.date2;
   todayDate = this.CurrDate.getUTCFullYear()+"-0"+this.month+"-"+this.date.slice(-2);
   generatedAt = this.CurrDate.getUTCFullYear()+"-0"+this.month+"-"+this.date.slice(-2)+"T10:40:22.755488303+01:00";
   activationDate = this.CurrDate.getUTCFullYear()+"-0"+this.month+"-"+this.date.slice(-2)+"T11:01:55+01:00";
   ceaseDate = this.CurrDate.getUTCFullYear()+"-0"+this.month+"-"+this.date.slice(-2)+"T14:37:21Z";
+  deliveryDate = this.CurrDate.getUTCFullYear()+"-0"+this.month+"-"+this.date3.slice(-2)+"T11:01:55+01:00"
 
   BOS_Validation='';
   Data_Accepted = '';
@@ -44,12 +48,19 @@ export class AppComponent {
   Voice_Activated = '';
   Data_Completed = '';
   Voice_Completed = '';
+  Sent_to_FH = '';
+  Accepted_By_FH = '';
+  Dispatched = '';
+  Delivered = '';
+  UnDelivered = '';
   Cease_Data_Warning = '';
   Cease_Data_Completed = ''
   Cease_Voice_Warning = '';
   Cease_Voice_Completed = '';
+  Return_received = '';
 
   manipulateJSON(){
+    console.log(this.date3);
     this.BOS_Validation = JSON.stringify({
       "id" : "cde8e672-e92b-4311-b2f7-8b53ecfec7aa",
       "correlationId" : "ID-bos-notification-service-bosd62-0-0-9-1690556292-6477954f8gl692-1690556872354-0-181",
@@ -200,11 +211,114 @@ export class AppComponent {
       "code" : "endUser.line.ceased",
       "message" : "The end user is ceasing their line. This will cease their current service."
     }, null, '\n');
+    
+    this.Sent_to_FH = JSON.stringify({
+      "id": "3119365d-6744-44c1-af8a-56d82fea4508",
+      "correlationId": "ID-bos-notification-service-bosd62-0-0-9-1690556292-6477954f8gl692-1690556872354-0-193",
+      "generatedAt": this.generatedAt,
+      "userId": "middleware",
+      "type": "SENT_TO_FULFILMENT_HOUSE",
+      "providerId": "DFS",
+      "operatorOrderId": this.formData.ooid,
+      "effectiveDateTime": this.activationDate,
+      "product": {
+        "id": this.formData.assetId
+      },
+      "trackingNotificationsExpected": false,
+      "fulfilmentHouseName": "Unipart",
+      "fulfilmentHouseReference": "145930926",
+      "fulfilmentHouseBatchReference": "N/A"
+    }, null, '\n');
+
+    this.Accepted_By_FH = JSON.stringify({
+      "id" : "887b3261-6801-4233-af4c-ae911bd093f7",
+      "correlationId" : "ID-bos-notification-service-bosd62-0-0-9-1690556292-6477954f8gl692-1690556872354-0-221",
+      "generatedAt" : this.generatedAt,
+      "userId" : "middleware",
+      "type" : "ACCEPTED_BY_FULFILMENT_HOUSE",
+      "providerId" : "DFS",
+      "operatorOrderId" : this.formData.ooid,
+      "product": {
+            "id": this.formData.assetId
+        },
+      "effectiveDateTime" : this.activationDate,
+      "trackingNotificationsExpected" : false,
+      "expectedDispatchDate" : this.activationDate,
+      "expectedDeliveryDate" : this.deliveryDate,
+      "stockAvailableDate" : this.activationDate
+    },null,'\n')
+
+    this.Dispatched = JSON.stringify({
+      "id" : "76299f75-07e0-46d9-b872-145ad0ec9a89",
+      "correlationId" : "ID-bos-notification-service-bosd62-0-0-9-1690556292-6477954f8gl692-1690556872354-0-205",
+      "generatedAt" : this.generatedAt,
+      "userId" : "middleware",
+      "type" : "DISPATCHED",
+      "providerId" : "DFS",
+      "operatorOrderId" : this.formData.ooid,
+      "effectiveDateTime" : this.activationDate,
+      "product": {
+            "id": this.formData.assetId
+        },
+      "trackingNotificationsExpected" : true,
+      "trackingCourier" : "ROYALMAIL24",
+      "trackingNumber" : "FR10552310GB",
+      "trackingUrl" : "http://www.royalmail.com/portal/rm/track?trackNumber=FR10552310GB",
+      "expectedDeliveryDate" : this.deliveryDate
+    },null,'\n')
+
+    this.Delivered = JSON.stringify({
+      "id" : "9dc63f1e-29e8-40df-8c6f-d5698b2e3160",
+      "correlationId" : "ID-bos-notification-service-bosd62-0-0-9-1690556292-6477954f8gl692-1690556872354-0-203",
+      "generatedAt" : this.generatedAt,
+      "userId" : "middleware",
+      "type" : "DELIVERED",
+      "providerId" : "DFS",
+      "operatorOrderId" : this.formData.ooid,
+      "product": {
+            "id": this.formData.assetId
+        },
+      "effectiveDateTime" : this.activationDate,
+      "trackingNotificationsExpected" : false
+    },null,'\n')
+
+    this.UnDelivered = JSON.stringify({
+      "id" : "aadc2f72-17ef-4d29-b208-642961775a63",
+      "correlationId" : "ID-bos-notification-service-bosd62-0-0-9-1691475112-c77cb7446w9kzp-1691481141660-0-31",
+      "generatedAt" : this.generatedAt,
+      "userId" : "middleware",
+      "type" : "HARDWARE_UNDELIVERED",
+      "providerId" : "DFS",
+      "product": {
+            "id": this.formData.assetId
+        }, 
+      "operatorOrderId" : this.formData.ooid,
+      "effectiveDateTime" : this.activationDate,
+      "trackingNotificationsExpected" : false,
+      "reason" : "Order Undelivered due to customer unavailability"
+    },null,'\n')
+
+    this.Return_received = JSON.stringify({
+      "id" : "12390ebf-852a-4ca5-9391-c1e6808e33f4",
+      "correlationId" : "ID-bos-notification-service-bosd62-0-0-9-1691475112-c77cb7446w9kzp-1691481141660-0-15",
+      "generatedAt" : this.generatedAt,
+      "userId" : "middleware",
+      "type" : "RETURN_RECEIVED",
+      "providerId" : "DFS",
+      "operatorOrderId" : this.formData.ooid,
+      "product": {
+            "id": this.formData.assetId
+        }, 
+      "effectiveDateTime" : this.activationDate,
+      "trackingNotificationsExpected" : false,
+      "trackingNumber" : "ROYALMAIL-12355"
+    },null,'\n')
+
   }
 
   onSubmit(){
     this.manipulateJSON();
-    if(this.formData.ooid!=''||this.formData.void!=''||this.formData.doid!=''||this.formData.vsid!=''||this.formData.dsid!=''||this.formData.directory!='')
+    if(this.formData.ooid!=''||this.formData.void!=''||this.formData.doid!=''||this.formData.vsid!=''||this.formData.dsid!=''||this.formData.directory!=''||this.formData.assetId!='')
       this.show_json = true;
   }
 
